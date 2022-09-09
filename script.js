5532686206892edfbe6fbf2b2adb0502e9563c00
Emerson Souza <souzaemerson003@gmail.com>
2022-09-09 09:42:47 -0300

    bug da master, re-up

---
+++ script.js
@@ -0,0 +1,38 @@
+let firstMatch = "";
+let secondMatch = "";
+let thirdMatch = "";
+let fourthMatch = "";
+let fifthMatch = "";
+
+let attemptArray = "";
+
+let letrecoDoDia = ["v", "a", "s", "c", "o"]
+
+let matchConfig = "";
+
+function match() {
+    inputOnefirstMatch = document.getElementById("firstLetter").value;
+    inputOneSecondMatch = document.getElementById("secondLetter").value;
+    inputOneThirdMatch = document.getElementById("thirdLetter").value;
+    inputOneFourthMatch = document.getElementById("fourthLetter").value;
+    inputOneFifthMatch = document.getElementById("fifthLetter").value;
+
+    attemptArray = [inputOnefirstMatch, inputOneSecondMatch, inputOneThirdMatch, inputOneFourthMatch, inputOneFifthMatch]
+    console.log(attemptArray);
+
+        matchConfig = letrecoDoDia.indexOf(inputOnefirstMatch);
+        console.log(matchConfig)
+
+        if (matchConfig <= 0) {
+            let element = document.getElementById("firstLetter");
+            element.style.backgroundColor = "#00ff00";
+            console.log("ok")
+    
+        }
+        else {
+            let element = document.getElementById("firstLetter");
+            element.style.backgroundColor = "#ff0000";
+            console.log("não encontrado")
+        }
+
+}
