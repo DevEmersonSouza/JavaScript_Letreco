1a6448f2264405947b32fc8579047d8b6d910e01
Emerson Souza <souzaemerson003@gmail.com>
2022-10-08 14:23:44 -0300

    projeto para enviar

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 let letrecoDoDia = "talco";
 let count = 5;
 let inputs = document.querySelectorAll("#inputsRow input");
-let tentativas = []
+let tentativas = [];
 
 // VERIFICAÇÃO PARA IDENTIFICAR SE A PALAVRA TEM 5 LETRAS
 function verificarInputs() {
@@ -28,11 +28,11 @@ function verificarInputs() {
 }
 
 function clearButton() {
-    firstLetter.value = ""
-    secondLetter.value = ""
-    thirdLetter.value = ""
-    fourthLetter.value = ""
-    fifthLetter.value = ""
+    firstLetter.value = "";
+    secondLetter.value = "";
+    thirdLetter.value = "";
+    fourthLetter.value = "";
+    fifthLetter.value = "";
 
 
     firstLetter.style.backgroundColor = "#ffffff";
@@ -41,11 +41,11 @@ function clearButton() {
     fourthLetter.style.backgroundColor = "#ffffff";
     fifthLetter.style.backgroundColor = "#ffffff";
 
-    firstLetter.disabled = false
-    secondLetter.disabled = false
-    thirdLetter.disabled = false
-    fourthLetter.disabled = false
-    fifthLetter.disabled = false
+    firstLetter.disabled = false;
+    secondLetter.disabled = false;
+    thirdLetter.disabled = false;
+    fourthLetter.disabled = false;
+    fifthLetter.disabled = false;
 
     firstLetter.focus();
 }
@@ -53,15 +53,15 @@ function clearButton() {
 //PASSAR AUTOMATICAMENTE PARA O PROXIMO INPUT
 function proximoInput() {
     if (firstLetter.maxlength === 1) {
-        secondLetter.focus()
+        secondLetter.focus();
         if (secondLetter.maxlength === 1) {
-            thirdLetter.focus()
+            thirdLetter.focus();
             if (thirdLetter.maxlength === 1) {
-                fourthLetter.focus()
+                fourthLetter.focus();
                 if (fourthLetter.maxlength === 1) {
-                    fifthLetter.focus()
+                    fifthLetter.focus();
                     if (fourthLetter.maxlength === 1) {
-                        fifthLetter.focus()
+                        fifthLetter.focus();
                     }
                 }
             }
@@ -72,97 +72,98 @@ function proximoInput() {
 
 
 function match() {
-    let primeiraTentativa = [document.querySelector("#firstLetter").value.toLowerCase(), document.querySelector("#secondLetter").value.toLowerCase(), document.querySelector("#thirdLetter").value.toLowerCase(), document.querySelector("#fourthLetter").value.toLowerCase(), document.querySelector("#fifthLetter").value.toLowerCase()]
-    console.log(primeiraTentativa)
+    let primeiraTentativa = [document.querySelector("#firstLetter").value.toLowerCase(), document.querySelector("#secondLetter").value.toLowerCase(), document.querySelector("#thirdLetter").value.toLowerCase(), document.querySelector("#fourthLetter").value.toLowerCase(), document.querySelector("#fifthLetter").value.toLowerCase()];
+    console.log(primeiraTentativa);
     firstLetter.style.backgroundColor = "#ff0000";
     secondLetter.style.backgroundColor = "#ff0000";
     thirdLetter.style.backgroundColor = "#ff0000";
     fourthLetter.style.backgroundColor = "#ff0000";
     fifthLetter.style.backgroundColor = "#ff0000";
 
-    let match1 = letrecoDoDia.indexOf(primeiraTentativa[0])
-    let match2 = letrecoDoDia.indexOf(primeiraTentativa[1])
-    let match3 = letrecoDoDia.indexOf(primeiraTentativa[2])
-    let match4 = letrecoDoDia.indexOf(primeiraTentativa[3])
-    let match5 = letrecoDoDia.indexOf(primeiraTentativa[4])
-    let matchGeral = [match1, match2, match3, match4, match5]
-    console.log(matchGeral)
+    let match1 = letrecoDoDia.indexOf(primeiraTentativa[0]);
+    let match2 = letrecoDoDia.indexOf(primeiraTentativa[1]);
+    let match3 = letrecoDoDia.indexOf(primeiraTentativa[2]);
+    let match4 = letrecoDoDia.indexOf(primeiraTentativa[3]);
+    let match5 = letrecoDoDia.indexOf(primeiraTentativa[4]);
+    let matchGeral = [match1, match2, match3, match4, match5];
+    console.log(matchGeral);
 
     if (firstLetter.value === "t" && secondLetter.value === "a" && thirdLetter.value === "l" && fourthLetter.value === "c" && fifthLetter.value === "o") {
         Swal.fire(
             'Parabéns!',
             'voce ganhou um bombom<br>(e o jogo também)<br>(mas nao me pergunte quem vai dar ele, sou apens um alert :))',
-        )
+        );
     }
     if (matchGeral.includes(-1)) {
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
             text: 'Para de fracassar e tenta de novo!',
-        })
+        });
 
+        setTimeout(clearButton(),3000);
     }
 
     //ACHANDO A LETRA CERTA NO INDICE [0]
     if (matchGeral.indexOf(0) === 0) {
         let element = document.getElementById("firstLetter");
         element.style.backgroundColor = "#00ff00";
-        console.log("encontrado no local certo")
+        console.log("encontrado no local certo");
     }
     // //ACHANDO A LETRA CERTA NO INDICE [1]
     if (matchGeral.indexOf(1) === 1) {
         let element = document.getElementById("secondLetter");
         element.style.backgroundColor = "#00ff00";
-        console.log("encontrado no local certo")
+        console.log("encontrado no local certo");
     }
     //ACHANDO A LETRA CERTA NO INDICE [2]
     if (matchGeral.indexOf(2) === 2) {
         let element = document.getElementById("thirdLetter");
         element.style.backgroundColor = "#00ff00";
-        console.log("encontrado no local certo")
+        console.log("encontrado no local certo");
     }
     //ACHANDO A LETRA CERTA NO INDICE [3]
     if (matchGeral.indexOf(3) === 3) {
         let element = document.getElementById("fourthLetter");
         element.style.backgroundColor = "#00ff00";
-        console.log("encontrado no local certo")
+        console.log("encontrado no local certo");
     }
     //ACHANDO A LETRA CERTA NO INDICE [4]
     if (matchGeral.indexOf(4) === 4) {
         let element = document.getElementById("fifthLetter");
         element.style.backgroundColor = "#00ff00";
-        console.log("encontrado no local certo")
+        console.log("encontrado no local certo");
     }
 
     //ACHANDO A LETRA ERRADA NO INDICE [0]
     if (matchGeral.indexOf(-1, [0]) === 0) {
         let element = document.getElementById("firstLetter");
         element.style.backgroundColor = "#ff0000";
-        console.log("não encontrado")
+        console.log("não encontrado");
     }
     //ACHANDO A LETRA ERRADA NO INDICE [1]
     if (matchGeral.indexOf(-1, [1]) === 1) {
         let element = document.getElementById("secondLetter");
         element.style.backgroundColor = "#ff0000";
-        console.log("não encontrado")
+        console.log("não encontrado");
     }
     //ACHANDO A LETRA ERRADA NO INDICE [2]
     if (matchGeral.indexOf(-1, [2]) === 2) {
         let element = document.getElementById("thirdLetter");
         element.style.backgroundColor = "#ff0000";
-        console.log("não encontrado")
+        console.log("não encontrado");
     }
     //ACHANDO A LETRA ERRADA NO INDICE [3]
     if (matchGeral.indexOf(-1, [3]) === 3) {
         let element = document.getElementById("fourthLetter");
         element.style.backgroundColor = "#ff0000";
-        console.log("não encontrado")
+        console.log("não encontrado");
     }
     //ACHANDO A LETRA ERRADA NO INDICE [4]
     if (matchGeral.indexOf(-1, [4]) === 4) {
         let element = document.getElementById("fifthLetter");
         element.style.backgroundColor = "#ff0000";
-        console.log("não encontrado")
+        console.log("não encontrado");
     }
 
 
@@ -170,33 +171,35 @@ function match() {
     if (matchGeral.indexOf(1) === 0 || matchGeral.indexOf(2) === 0 || matchGeral.indexOf(3) === 0 || matchGeral.indexOf(4) === 0) {
         let element = document.getElementById("firstLetter");
         element.style.backgroundColor = "#ffff00";
-        console.log("encontrado mas no local errado")
+        console.log("encontrado mas no local errado");
     }
     if (matchGeral.indexOf(0) === 1 || matchGeral.indexOf(2) === 1 || matchGeral.indexOf(3) === 1 || matchGeral.indexOf(4) === 1) {
         let element = document.getElementById("secondLetter");
         element.style.backgroundColor = "#ffff00";
-        console.log("encontrado mas no local errado")
+        console.log("encontrado mas no local errado");
     }
     if (matchGeral.indexOf(0) === 2 || matchGeral.indexOf(1) === 2 || matchGeral.indexOf(3) === 2 || matchGeral.indexOf(4) === 2) {
         let element = document.getElementById("thirdLetter");
         element.style.backgroundColor = "#ffff00";
-        console.log("encontrado mas no local errado")
+        console.log("encontrado mas no local errado");
     }
     if (matchGeral.indexOf(0) === 3 || matchGeral.indexOf(1) === 3 || matchGeral.indexOf(2) === 3 || matchGeral.indexOf(4) === 3) {
         let element = document.getElementById("fourthLetter");
         element.style.backgroundColor = "#ffff00";
-        console.log("encontrado mas no local errado")
+        console.log("encontrado mas no local errado");
     }
     if (matchGeral.indexOf(0) === 4 || matchGeral.indexOf(1) === 4 || matchGeral.indexOf(2) === 4 || matchGeral.indexOf(3) === 4) {
         let element = document.getElementById("fifthLetter");
         element.style.backgroundColor = "#ffff00";
-        console.log("encontrado mas no local errado")
+        console.log("encontrado mas no local errado");
     }
-    firstLetter.disabled = true
-    secondLetter.disabled = true
-    thirdLetter.disabled = true
-    fourthLetter.disabled = true
-    fifthLetter.disabled = true
+    firstLetter.disabled = true;
+    secondLetter.disabled = true;
+    thirdLetter.disabled = true;
+    fourthLetter.disabled = true;
+    fifthLetter.disabled = true;
+
+    
 }
 
 
@@ -209,6 +212,12 @@ function adicionar() {
 
     nomeDig.innerHTML = primeiraTentativaArray;
     lista.appendChild(nomeDig);
+
+    let backgroundColor = document.querySelector("#firstLetter").style.backgroundColor,    
+    targets = document.querySelectorAll("nomeDig");
+
+for(var i = 0; i < targets.length; i++)
+  targets[i].style.color = backgroundColor;
 }
 
 
@@ -224,7 +233,12 @@ function contador() {
             icon: 'error',
             title: 'Oops...',
             text: 'Suas Tentativas Acabaram(GAME OVER)',
-        })
+        });
+        firstLetter.disabled = true;
+        secondLetter.disabled = true;
+        thirdLetter.disabled = true;
+        fourthLetter.disabled = true;
+        fifthLetter.disabled = true;
         
     }
 
@@ -233,29 +247,29 @@ function contador() {
 function keyboard(value) {
     if (inputs[0].value === "") {
         inputs[0].value = inputs[0].value + value;
-        inputs[1].focus()
+        inputs[1].focus();
         return
     }
     if (inputs[1].value === "") {
         inputs[1].value = inputs[1].value + value;
-        inputs[2].focus()
+        inputs[2].focus();
         return
     }
     if (inputs[2].value === "") {
         inputs[2].value = inputs[2].value + value;
-        inputs[3].focus()
+        inputs[3].focus();
         return
     }
     if (inputs[3].value === "") {
         inputs[3].value = inputs[3].value + value;
-        inputs[4].focus()
+        inputs[4].focus();
         return
     }
     if (inputs[4].value === "") {
         inputs[4].value = inputs[4].value + value;
     }
     if (inputs[4].value != "") {
-        inputs[4].focus()
+        inputs[4].focus();
     }
 }
 
